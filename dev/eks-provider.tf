ae99eb7f7a9693984929144604ac42e58f99d965
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2024-08-15 21:23:55 -0500

    Update eks-provider.tf

--- dev/eks-provider.tf
+++ dev/eks-provider.tf
@@ -1,6 +1,12 @@
-data "aws_eks_cluster" "my_cluster" {
-   name = "test"
+data "aws_eks_cluster"  "my_cluster" {
+   name         = "test"
 }
+
+# obtain a cluster token for providers, tokens are short lived (15 minutes)
+data "aws_eks_cluster_auth" "cluster_auth" {
+  name = data.aws_eks_cluster.my_cluster.name
+}
+
 
 output "endpoint" {
   value = data.aws_eks_cluster.my_cluster.endpoint
@@ -10,33 +16,32 @@ output "kubeconfig-certificate-authority-data" {
   value = data.aws_eks_cluster.my_cluster.certificate_authority[0].data
 }
 
-# Only available on Kubernetes version 1.13 and 1.14 clusters created or upgraded on or after September 3, 2019.
 output "identity-oidc-issuer" {
   value = data.aws_eks_cluster.my_cluster.identity[0].oidc[0].issuer
 }
 
+locals {
+  aws_region   = "us-east-1"
+}
+
+######################################################################################
+# K8S and Helm Provider
+######################################################################################
+
 provider "kubernetes" {
-    host = "${data.aws_eks_cluster.my_cluster.endpoint}"
-    cluster_ca_certificate = "${data.aws_eks_cluster.my_cluster.certificate_authority[0].data}"
-    exec {
-        api_version = "client.authentication.k8s.io/v1beta1"
-        args    = [ "eks", "get-token", "--cluster-name", "${data.aws_eks_cluster.my_cluster.name}", "--region", "us-east-1" ]
-        command = aws
-    } 
+  host                   = "${data.aws_eks_cluster.my_cluster.endpoint}"
+  cluster_ca_certificate = base64decode("${data.aws_eks_cluster.my_cluster.certificate_authority[0].data}")
+  token                  = data.aws_eks_cluster_auth.cluster_auth.token
 }
 
 provider "helm" {
-    kubernetes {
-        host = "${data.aws_eks_cluster.my_cluster.endpoint}"
-        cluster_ca_certificate = "${data.aws_eks_cluster.my_cluster.certificate_authority[0].data}"
-        exec {
-            api_version = "client.authentication.k8s.io/v1beta1"
-            args    = [ "eks", "get-token", "--cluster-name", "${data.aws_eks_cluster.my_cluster.name}", "--region", "us-east-1" ]
-            command = aws
-        } 
-    }    
-}
+  kubernetes {
+    host                   = "${data.aws_eks_cluster.my_cluster.endpoint}"
+    cluster_ca_certificate = base64decode("${data.aws_eks_cluster.my_cluster.certificate_authority[0].data}")
+    token                  = data.aws_eks_cluster_auth.cluster_auth.token
+  }
+}  
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
