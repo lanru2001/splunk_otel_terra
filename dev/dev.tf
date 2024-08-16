20d3233cfbaf0712caf93dda897de52a2f31b8ad
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2024-08-15 21:25:37 -0500

    Update dev.tf

--- dev/dev.tf
+++ dev/dev.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
-        bucket = "swimlane_logging"
-        key = "project/swimlane/dev/logs"
+        bucket = "tf-remote-dev-bkt"
+        key    = "project/splunk-otel/ds"
         region = "us-east-1"
     }
 
@@ -30,15 +30,15 @@ module "dev_splunk_otel" {
     
     source          = "../../"
     cluster_name    = "test"
-    splunk_endpoint = ""
-    hec_token       = ""
+    splunk_endpoint = "https://prd-p-boa0r.splunkcloud.com:8088/services/collector/raw"
+    hec_token       = "6e81bca3-fe7a-4a0e-930a-1c2e3a52698f"
     index           = "eks"
     insecure_skip_verify = true
     fluentd_repo    = "splunk/fluentd-hec"
     fluentd_tag     = "1.3.3"
     fluentd_pullPolicy = "IfNotPresent"
     otelcol_repo    = "quay.io/signalfx/splunk-otel-collector"
-    otelcol_tag     = "0.91.3"
+    otelcol_tag     = "0.105.0"
     otelcol_pullPolicy = "IfNotPresent"
 
 }
